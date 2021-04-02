17583733b9c261eaf7ec83aeaa12b8d5ff5cd483
DolphaGo <adamdoha@naver.com>
2021-04-02 23:35:10 +0900

    settings for health-check

--- src/main/java/com/example/monitoring/controller/HelloController.java
+++ src/main/java/com/example/monitoring/controller/HelloController.java
@@ -1,2 +1,16 @@
-package com.example.monitoring.controller;public class HelloController {
+package com.example.monitoring.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public ResponseEntity hello(){
+        return ResponseEntity.ok("hello");
+    }
 }
